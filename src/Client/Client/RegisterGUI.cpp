2a457bcfdf52291678c3ee9dbd9256182a7e7679
Daniele Amenta <daniele.amenta@hotmail.com>
2021-12-29 09:51:21 +0100

    Client - RegisterGUI

--- src/Client/Client/RegisterGUI.cpp
+++ src/Client/Client/RegisterGUI.cpp
@@ -16,6 +16,8 @@ RegisterGUI::RegisterGUI(QWidget* parent)
 	ui.pswErrorLabel->setText(QString::fromStdString(""));
 	ui.phoneErrorLabel->setText(QString::fromStdString(""));
 	ui.dateErrorLabel->setText(QString::fromStdString(""));
+
+	ui.dateEdit->setDisplayFormat(QString::fromStdString("yyyy.MM.dd"));
 }
 
 RegisterGUI::~RegisterGUI()
@@ -23,9 +25,33 @@ RegisterGUI::~RegisterGUI()
 }
 
 void RegisterGUI::on_backToLoginBtn_clicked() {
-
+	close();
 }
 
 void RegisterGUI::on_registerBtn_clicked() {
+	try {
+		json j;
+		j["name"] = ui.nameLineEdit->text().toStdString();
+		j["surname"] = ui.surnameLineEdit->text().toStdString();
+		j["email"] = ui.emailLineEdit->text().toStdString();
+		j["psw"] = ui.pswLineEdit->text().toStdString();
+		j["phone"] = ui.phoneLineEdit->text().toStdString();
+		j["birthDate"] = ui.dateEdit->date().toString(Qt::ISODate).toStdString();
 
+		// TODO: validation input fields
+		cpr::Response r = cpr::Post(cpr::Url{ serverUrl + "/user/registration" },
+			cpr::Body{ j.dump() },
+			cpr::Header{ { "Content-Type", "application/json" } });
+
+		if (r.status_code == 200) {
+			QMessageBox::information(this, "Login", QString::fromStdString("User create successfully!"));
+			close();
+		}
+		else {
+			ui.dateErrorLabel->setText(QString::fromStdString("An error occour!"));
+		}
+	}
+	catch (...) {
+		ui.dateErrorLabel->setText(QString::fromStdString("An error occour!"));
+	}
 }
