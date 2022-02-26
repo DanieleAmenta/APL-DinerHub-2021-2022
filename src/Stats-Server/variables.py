8b0d7dec815f35f5058640add87963c70c6103b8
Daniele Amenta <daniele.amenta@hotmail.com>
2022-02-26 11:50:29 +0100

    DinerHub: Python token validation

--- src/Stats-Server/variables.py
+++ src/Stats-Server/variables.py
@@ -2,6 +2,9 @@ from flask import Response, request
 from functools import wraps
 import jwt
 
+token_key = "St0nGKeE3Y-d1n3RHuB!-4578690012-S3RveR!0372983849102394A"
+token_audience = "http://localhost:5276"
+
 token_missing = "token is missing"
 token_not_valid = "token not valid"
 token_unauthorized = "unauthorized"
@@ -19,7 +22,10 @@ def token_required_admin(f):
         if not token:
             return f'{token_missing}', 403
         try:
-            data = jwt.decode(token, options={"verify_signature": False})
+            data = jwt.decode(token,
+                              token_key,
+                              audience=token_audience,
+                              algorithms=["HS256"])
             role = data["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
             if role != role_admin:
                 return f'{token_unauthorized}', 401
@@ -39,7 +45,10 @@ def token_required_restaurant(f):
         if not token:
             return f'{token_missing}', 403
         try:
-            data = jwt.decode(token, options={"verify_signature": False})
+            data = jwt.decode(token,
+                              token_key,
+                              audience=token_audience,
+                              algorithms=["HS256"])
             role = data["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
             if role != role_admin and role != role_restaurant:
                 return f'{token_unauthorized}', 401
@@ -59,7 +68,10 @@ def token_required_user(f):
         if not token:
             return f'{token_missing}', 403
         try:
-            data = jwt.decode(token, options={"verify_signature": False})
+            data = jwt.decode(token,
+                              token_key,
+                              audience=token_audience,
+                              algorithms=["HS256"])
             role = data["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
             if role != role_admin and role != role_user:
                 return f'{token_unauthorized}', 401
